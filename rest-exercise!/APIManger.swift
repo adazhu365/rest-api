16a61e692fb921a3f7183551ad67eef183cc1bca
adazhu365 <gz6xw@virginia.edu>
2018-10-12 00:29:30 -0400

    new file

--- rest-exercise!/APIManger.swift
+++ rest-exercise!/APIManger.swift
@@ -4,11 +4,14 @@ import SwiftyJSON
 class APIManager: NSObject {
     
     let baseURL = "http://stardock.cs.virginia.edu/louslist/Courses/view/CS"
+    //    let baseURL = "http://devhub.virginia.edu/api/5bb762cd5e09334d77897b41/buildings"
     static let sharedInstance = APIManager()
     
     func getPostWithId(postId: String, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
     
         let url : String = baseURL + String("/" + postId + "?json")
+        //        let url : String = baseURL + String('/"' + postId + '"')
+
         
         
     let request: NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
